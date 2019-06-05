ea9468b466a598b16c46fd17048be492e3de9e2a
jmoh3 <jmoh3@illinois.edu>
2019-06-04 23:32:04 -0500

    Drafted treebuilder

--- script.py
+++ script.py
@@ -1,27 +1,29 @@
+import copy
+
 # Finds the last inversion (i, j) of a permutation with the following criteria:
 # a pair (i,j) with i < j and w(i) > w(j) that maximizes i, then j
-def findLastInversion(perm):
+def findLastInversion(w):
 
   biggestI = -1
   biggestJ = -1
   
-  for i in range(0, len(perm)):
-    for j in range(i, len(perm)):
-      if perm[j] < perm[i] and (i > biggestI or (i == biggestI and j > biggestJ)):
+  for i in range(0, len(w)):
+    for j in range(i, len(w)):
+      if w[j] < w[i] and (i > biggestI or (i == biggestI and j > biggestJ)):
         biggestI = i
         biggestJ = j
   
   return (biggestI, biggestJ)
 
 # Given a permutation and an inversion, find the pivots of the inversion
-def findPivots(perm, i, j):
+def findPivots(w, i, j):
   pivots = []
 
   for h in range(i - 1, 0, -1):
-    if perm[h] < perm[j]:
+    if w[h] < w[j]:
       isValid = True
       for hPrime in range(h, i):
-        if perm[h] < perm[hPrime] and perm[hPrime] < perm[j]:
+        if w[h] < w[hPrime] and w[hPrime] < w[j]:
           isValid = False
           break
       if isValid:
@@ -29,11 +31,34 @@ def findPivots(perm, i, j):
   
   return pivots
 
-def findChildren(perm):
-  return None
+def findChildren(w, i, j, pivots):
+  children = []
 
-w = [2, 1, 5, 4, 6, 3]
+  for pivot in pivots:
+    tmpI = w[i]
+    tmpJ = w[j]
+    tmpH = w[pivot]
 
-i, j = findLastInversion(w)
+    child = copy.deepcopy(w)
+    child[pivot] = tmpJ
+    child[i] = tmpH
+    child[j] = tmpI
+    children.append(child)
 
-print(findPivots(w, i, j))
+  return children
+
+def buildTree(w):
+  print("called")
+  i, j = findLastInversion(w)
+  pivots = findPivots(w, i, j)
+
+  if len(pivots) == 0:
+    print(w)
+  else:
+    children = findChildren(w, i, j, pivots)
+    for child in children:
+      buildTree(child)
+
+
+w = [5, 4, 2, 7, 8, 3, 1, 6]
+buildTree(w)
