f52b3d4778f6fdaa6568902af32e70f474791911
jmoh3 <jmoh3@illinois.edu>
2019-09-21 15:04:02 -0500

    Added function to find accessible box

---
+++ find_accessible_box.py
@@ -0,0 +1,17 @@
+def find_accessible_box(l):
+    accessible_box = 0
+    last_dominant = -1
+    accessible_box = None
+    found_inversion = False
+
+    for i in range(1, len(l)):
+        if found_inversion and l[i] - last_dominant > 0:
+            accessible_box = i
+        elif not found_inversion and l[i] > l[i-1]:
+            found_inversion = True
+            accessible_box = i
+            last_dominant = l[i-1]
+    
+    return accessible_box
+
+print(find_accessible_box([4, 3, 1, 3, 1, 1]))
