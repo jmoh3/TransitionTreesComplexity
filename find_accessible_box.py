214d6fce098187621611262c75416b892da6f0c3
jmoh3 <jmoh3@illinois.edu>
2019-10-29 13:36:36 -0500

    Fix accessible box code

--- find_accessible_box.py
+++ find_accessible_box.py
@@ -7,13 +7,13 @@ def find_accessible_box(l):
     for i in range(1, len(l)):
         if found_inversion and l[i] - last_dominant > 0:
             accessible_box = i
-        elif l[i] > l[i-1]:
+        if l[i] > l[i-1]:
             found_inversion = True
             accessible_box = i
             last_dominant = l[i-1]
     
     return accessible_box
 
-# print(find_accessible_box([2, 2, 3, 1, 2, 1, 1]))
+print(find_accessible_box([3, 3, 3, 7, 0, 6, 2]))
 
 # print(find_accessible_box([4, 2, 0, 3]))
